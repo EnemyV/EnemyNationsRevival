38c871c07af0f6d95c4d3261225436dc80e3ac52
neverdot <virgintier707@gmail.com>
2026-04-13 13:24:19 -0700

    Phase 2: replace CDlgLicense with non-MFC Win32 MessageBox implementation

--- enations_latest/src/license.cpp
+++ enations_latest/src/license.cpp
@@ -1,5 +1,6 @@
 // License.cpp : implementation file
 //
+// CDlgLicense — Non-MFC license dialog.
 
 #include "stdafx.h"
 #include "lastplnt.h"
@@ -11,81 +12,48 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-/////////////////////////////////////////////////////////////////////////////
-// CDlgLicense dialog
-
-
-CDlgLicense::CDlgLicense(int iText, BOOL bOK, CWnd* pParent /*=NULL*/)
-	: CDialog(CDlgLicense::IDD, pParent)
+CDlgLicense::CDlgLicense(int iText, BOOL bOK, CWnd* /*pParent*/)
 {
-
-	m_iText = iText;
-	m_bOK = bOK;
-
-	//{{AFX_DATA_INIT(CDlgLicense)
-	m_strLicense = _T("");
-	//}}AFX_DATA_INIT
+    m_iText = iText;
+    m_bOK = bOK;
 }
 
-
-void CDlgLicense::DoDataExchange(CDataExchange* pDX)
+int CDlgLicense::DoModal()
 {
-	CDialog::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(CDlgLicense)
-	DDX_Control(pDX, IDYES, m_btnYes);
-	DDX_Control(pDX, IDOK, m_btnOk);
-	DDX_Control(pDX, IDCANCEL, m_btnCancel);
-	DDX_Text(pDX, IDC_LICENSE, m_strLicense);
-	//}}AFX_DATA_MAP
-}
+    // Load license text from data file (LANG/LEGL/LICn chunk)
+    CString strLicense;
+    try
+    {
+        CMmio* pMmio = theDataFile.OpenAsMMIO( NULL, "LANG" );
+        pMmio->DescendRiff( 'L', 'A', 'N', 'G' );
+        pMmio->DescendList( 'L', 'E', 'G', 'L' );
+        pMmio->DescendChunk( 'L', 'I', 'C', (char)( '0' + m_iText ) );
 
+        long lSize = pMmio->ReadLong();
+        pMmio->Read( strLicense.GetBuffer( lSize + 2 ), lSize );
+        strLicense.ReleaseBuffer( lSize );
 
-BEGIN_MESSAGE_MAP(CDlgLicense, CDialog)
-	//{{AFX_MSG_MAP(CDlgLicense)
-	ON_BN_CLICKED(IDYES, OnYes)
-	//}}AFX_MSG_MAP
-END_MESSAGE_MAP()
+        delete pMmio;
+    }
+    catch ( ... )
+    {
+        strLicense = "(License text unavailable)";
+    }
 
-/////////////////////////////////////////////////////////////////////////////
-// CDlgLicense message handlers
+    UINT uType = MB_TASKMODAL;
+    if ( m_bOK )
+        uType |= MB_OK | MB_ICONINFORMATION;
+    else
+        uType |= MB_YESNO | MB_ICONINFORMATION;
 
-BOOL CDlgLicense::OnInitDialog() 
-{
-	CDialog::OnInitDialog();
-	
-	if ( m_bOK )
-		{
-		m_btnYes.EnableWindow (FALSE);
-		m_btnYes.ShowWindow (SW_HIDE);
-		m_btnCancel.EnableWindow (FALSE);
-		m_btnCancel.ShowWindow (SW_HIDE);
-		}
-	else
-		{
-		m_btnOk.EnableWindow (FALSE);
-		m_btnOk.ShowWindow (SW_HIDE);
-		}
+    int iRtn = ::MessageBoxA( NULL, (const char*)strLicense, "Enemy Nations — License", uType );
 
-	// set the string
-	CMmio *pMmio = theDataFile.OpenAsMMIO (NULL, "LANG");
-	pMmio->DescendRiff ('L', 'A', 'N', 'G');
-	pMmio->DescendList ('L', 'E', 'G', 'L');
-	pMmio->DescendChunk ('L', 'I', 'C', '0' + m_iText);
+    // Map Yes -> OK for callers that check IDOK
+    if ( iRtn == IDYES )
+        iRtn = IDOK;
+    // Map No -> Cancel for callers that check IDCANCEL
+    if ( iRtn == IDNO )
+        iRtn = IDCANCEL;
 
-	long lSize = pMmio->ReadLong ();
-	pMmio->Read (m_strLicense.GetBuffer (lSize+2), lSize);
-
-	delete pMmio;
-	m_strLicense.ReleaseBuffer (lSize);
-
-	UpdateData (FALSE);
-	
-	return TRUE;  // return TRUE unless you set the focus to a control
-	              // EXCEPTION: OCX Property Pages should return FALSE
-}
-
-void CDlgLicense::OnYes() 
-{
-
-	EndDialog (IDOK);	
+    return iRtn;
 }
